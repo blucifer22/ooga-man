08f0e9270fa549d319dfaf172ba4b3ee6b0062f8
Franklin Wei <franklin@rockbox.org>
2021-04-03 15:30:56 -0400

    Add implementations of onKeyPress, onKeyRelease

--- src/ooga/controller/HumanInputManager.java
+++ src/ooga/controller/HumanInputManager.java
@@ -5,4 +5,23 @@ import ooga.view.HumanInputConsumer;
 
 public class HumanInputManager implements InputSource, HumanInputConsumer {
   //TODO: Translate keypresses to inputs for sprites ex.) keypress ==> vec2
+  Set<KeyCode> pressedKeys;
+
+  @Override
+  public Vec2 getRequestedDirection() {
+    Vec2 ret;
+    // in case pressedKeys contains UP, ret += (1, 0)
+    // pressedKeys contains DOWN, ret -= (1, 0)
+
+  }
+
+  @Override
+  public void onKeyPress(KeyCode code) {
+    pressedKeys.add(code);
+  }
+
+  @Override
+  public void onKeyRelease(KeyCode code) {
+    pressedKeys.remove(code);
+  }
 }
