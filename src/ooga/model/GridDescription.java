b3892a6e93d65aaff520d14ff0bddedd816c971a
Marc Chmielewski <msc68@duke.edu>
2021-04-03 18:40:00 -0400

    Additional GridDescription documentation and tests

--- src/ooga/model/GridDescription.java
+++ src/ooga/model/GridDescription.java
@@ -11,17 +11,27 @@ import java.util.List;
  * @author Franklin Wei
  */
 public class GridDescription {
+
   private final int width;
   private final int height;
   private final Tile[][] grid;
 
+  /**
+   * The general constructor for GridDescription. Takes in a width, a height, and a 2D array of
+   * Tiles with which to initially populate itself.
+   *
+   * @param width The width of the Grid that this GridDescription represents.
+   * @param height The height of the Grid that this GridDescription represents.
+   * @param tileList The List of Tiles that compose the Grid that this GridDescription represents.
+   * @throws IllegalArgumentException If the wrong number of Tiles are provided
+   */
   public GridDescription(int width, int height, List<Tile> tileList)
       throws IllegalArgumentException {
 
     this.width = width;
     this.height = height;
 
-    if (tileList.size() > width * height) {
+    if (tileList.size() != width * height) {
       throw new IllegalArgumentException(
           "ILLEGAL ARGUMENT EXCEPTION:\nTOO MANY TILES FOR INDICATED DIMENSIONS!");
     }
@@ -34,14 +44,29 @@ public class GridDescription {
     }
   }
 
+  /**
+   * Get the width of this GridDescription.
+   *
+   * @return The width of this GridDescription.
+   */
   public int getWidth() {
     return width;
   }
 
+  /**
+   * Get the height of this GridDescription.
+   *
+   * @return The height of this GridDescription.
+   */
   public int getHeight() {
     return height;
   }
 
+  /**
+   * Get the 2D array of Tiles that make up this GridDescription
+   *
+   * @return A 2D array of Tiles that make up this GridDescription
+   */
   public Tile[][] getGrid() {
     return grid;
   }
--- test/GridDescriptionTests.java
+++ test/GridDescriptionTests.java
@@ -38,9 +38,25 @@ public class GridDescriptionTests {
   }
 
   @Test
-  public void testGridDescriptionExceptions() {
+  public void testGridDescriptionOverflowException() {
     // Simulate loading in a small grid from JSON
     int width = 1;
+    int height = 1;
+    List<Tile> tileList =
+        List.of(
+            new Tile(new TileCoordinates(0, 0), "Tile 0", false),
+            new Tile(new TileCoordinates(1, 0), "Tile 1", true),
+            new Tile(new TileCoordinates(0, 1), "Tile 2", false),
+            new Tile(new TileCoordinates(1, 1), "Tile 3", false));
+
+    assertThrows(IllegalArgumentException.class, () -> {
+      GridDescription gridDescription = new GridDescription(width, height, tileList);
+    });
+  }
+
+  @Test public void testGridDescriptionUnderflowException() {
+    // Simulate loading in a small grid from JSON
+    int width = 10;
     int height = 1;
     List<Tile> tileList =
         List.of(
