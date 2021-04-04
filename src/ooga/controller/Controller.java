2b8550bfbd383fcdaa6f440984937fdd12e3234d
David Coffman <david.coffman@duke.edu>
2021-04-04 16:03:26 -0400

    Fix controller bug

--- src/ooga/controller/Controller.java
+++ src/ooga/controller/Controller.java
@@ -1,14 +1,12 @@
 package ooga.controller;
 
 import javafx.scene.Scene;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import ooga.model.PacmanGameState;
 import ooga.model.Sprite;
 import ooga.model.SpriteCoordinates;
 import ooga.model.TileCoordinates;
-import ooga.view.GameGridView;
 import ooga.view.GameView;
 
 public class Controller {
@@ -24,7 +22,7 @@ public class Controller {
     PacmanGameState pgs = new PacmanGameState();
     GameView gv = new GameView(10, 10);
     pgs.addExistenceObserver(gv);
-    primaryStage.setScene(new Scene((Pane) (new GameGridView(10,10).getRenderingNode()), 400,
+    primaryStage.setScene(new Scene((Pane) (new GameView(10,10).getRenderingNode()), 400,
         400));
     gv.onSpriteCreation(new Sprite() {
 
