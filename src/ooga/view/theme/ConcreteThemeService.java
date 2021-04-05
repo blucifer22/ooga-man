9e1b2a0cbb5991c16f9e66f42537ec20432b7697
David Coffman <david.coffman@duke.edu>
2021-04-05 17:03:06 -0400

    Refactor TileView into separate class

--- src/ooga/view/theme/ConcreteThemeService.java
+++ src/ooga/view/theme/ConcreteThemeService.java
@@ -20,6 +20,7 @@ public class ConcreteThemeService implements ThemeService {
     return switch (type) {
       case "pacman" -> Color.YELLOW;
       case "ghost" -> Color.RED;
+      case "tile" -> Color.GAINSBORO;
       default -> Color.BLUE;
     };
   }
