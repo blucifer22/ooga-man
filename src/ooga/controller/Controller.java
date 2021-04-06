4a02766ed34e92ba5e9124b8253d592062fc8327
Marc Chmielewski <msc68@duke.edu>
2021-04-06 14:52:05 -0400

    My MR wasn't finished. D'oh

--- src/ooga/controller/Controller.java
+++ src/ooga/controller/Controller.java
@@ -3,8 +3,6 @@ package ooga.controller;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import ooga.model.PacmanGameState;
-import ooga.model.PacmanGrid;
-import ooga.model.Sprite;
 import ooga.model.SpriteCoordinates;
 import ooga.model.api.ObservableSprite;
 import ooga.model.api.SpriteEvent.EventType;
@@ -29,72 +27,67 @@ public class Controller {
     ThemeService ts = new ConcreteThemeService();
     GameView gv = new GameView(ts);
     pgs.addSpriteExistenceObserver(gv.getSpriteExistenceObserver());
-    primaryStage.setScene(new Scene(gv.getRenderingNode(), 400.0,
-        400.0));
-    gv.getSpriteExistenceObserver().onSpriteCreation(new ObservableSprite() {
+    primaryStage.setScene(new Scene(gv.getRenderingNode(), 400.0, 400.0));
+    gv.getSpriteExistenceObserver()
+        .onSpriteCreation(
+            new ObservableSprite() {
 
-      @Override
-      public boolean isVisible() {
-        return true;
-      }
+              @Override
+              public boolean isVisible() {
+                return true;
+              }
 
-      @Override
-      public String getType() {
-        return "pacman";
-      }
+              @Override
+              public String getType() {
+                return "pacman";
+              }
 
-      @Override
-      public SpriteCoordinates getCenter() {
-        return new SpriteCoordinates(new Vec2(1.5, 3.5));
-      }
+              @Override
+              public SpriteCoordinates getCenter() {
+                return new SpriteCoordinates(new Vec2(1.5, 3.5));
+              }
 
-      @Override
-      public Vec2 getDirection() {
-        return new Vec2(1, 0);
-      }
+              @Override
+              public Vec2 getDirection() {
+                return new Vec2(1, 0);
+              }
 
-      @Override
-      public void addObserver(SpriteObserver so, EventType... observedEvents) {
+              @Override
+              public void addObserver(SpriteObserver so, EventType... observedEvents) {}
 
-      }
+              @Override
+              public void removeObserver(SpriteObserver so) {}
+            });
 
-      @Override
-      public void removeObserver(SpriteObserver so) {
+    gv.getSpriteExistenceObserver()
+        .onSpriteCreation(
+            new ObservableSprite() {
 
-      }
-    });
+              @Override
+              public boolean isVisible() {
+                return true;
+              }
 
-    gv.getSpriteExistenceObserver().onSpriteCreation(new ObservableSprite() {
+              @Override
+              public String getType() {
+                return "ghost";
+              }
 
-      @Override
-      public boolean isVisible() {
-        return true;
-      }
+              @Override
+              public SpriteCoordinates getCenter() {
+                return new SpriteCoordinates(new Vec2(1.5, 5.5));
+              }
 
-      @Override
-      public String getType() {
-        return "ghost";
-      }
+              @Override
+              public Vec2 getDirection() {
+                return new Vec2(1, 0);
+              }
 
-      @Override
-      public SpriteCoordinates getCenter() {
-        return new SpriteCoordinates(new Vec2(1.5, 5.5));
-      }
+              @Override
+              public void addObserver(SpriteObserver so, EventType... observedEvents) {}
 
-      @Override
-      public Vec2 getDirection() {
-        return new Vec2(1, 0);
-      }
-
-      @Override
-      public void addObserver(SpriteObserver so, EventType... observedEvents) {
-
-      }
-
-      @Override
-      public void removeObserver(SpriteObserver so) {
-
-      }
-    });
+              @Override
+              public void removeObserver(SpriteObserver so) {}
+            });
   }
 }
