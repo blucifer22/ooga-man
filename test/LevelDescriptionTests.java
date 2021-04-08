455e471249973b8b324af914cd51a8791c7dbd37
Marc Chmielewski <msc68@duke.edu>
2021-04-07 21:54:45 -0400

    Added full-depth LevelDescriptionJSON test

--- test/LevelDescriptionTests.java
+++ test/LevelDescriptionTests.java
@@ -45,4 +45,44 @@ public class LevelDescriptionTests {
     assertEquals(levelDescription.getGridDescription().getGridName(), "testGrid");
     assertEquals(spriteLayoutDescription.getSprites().get(0).getClassName(), "Ghost");
   }
+
+  @Test
+  public void testLevelDescriptionJSON() {
+    String gridDescriptionPath = "data/levels/grids/test_grid.json";
+    String spriteLayoutDescriptionPath = "data/levels/sprite-layouts/test_sprite_layout.json";
+    GridDescription gridDescription = null;
+    SpriteLayoutDescription spriteLayoutDescription = null;
+
+    try {
+      gridDescription = jsonDescriptionFactory.getGridDescriptionFromJSON(gridDescriptionPath);
+      spriteLayoutDescription =
+          jsonDescriptionFactory.getSpriteLayoutDescriptionFromJSON(spriteLayoutDescriptionPath);
+    } catch (IOException e) {
+      System.err.println(e.getMessage());
+      fail();
+    }
+
+    LevelDescription levelDescription =
+        new LevelDescription(gridDescription, spriteLayoutDescription);
+
+    assertEquals(levelDescription.getGridDescription().getGridName(), "testGrid");
+    assertEquals(spriteLayoutDescription.getSprites().get(0).getClassName(), "Ghost");
+
+    String levelDescriptionPath = "data/levels/test_level.json";
+    LevelDescription levelDescriptionFromJSON = null;
+    try {
+      levelDescription.toJSON(levelDescriptionPath);
+      levelDescriptionFromJSON =
+          jsonDescriptionFactory.getLevelDescriptionFromJSON(levelDescriptionPath);
+    } catch (IOException e) {
+      System.err.println(e.getMessage());
+      fail();
+    }
+
+    assertEquals(
+        levelDescriptionFromJSON.getGridDescription().getHeight(), gridDescription.getHeight());
+    assertEquals(
+        levelDescriptionFromJSON.getSpriteLayoutDescription().getSprites().size(),
+        spriteLayoutDescription.getSprites().size());
+  }
 }
