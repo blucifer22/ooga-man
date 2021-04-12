4ee27ecd4d338b1188aeaec27a022672ad534b62
George Hong <grh17@duke.edu>
2021-04-12 17:40:26 -0600

    Added list of prospective PacmanEventTypes

--- src/ooga/model/PacmanEventType.java
+++ src/ooga/model/PacmanEventType.java
@@ -1,6 +1,18 @@
 package ooga.model;
 
+/**
+ * PacmanEventTypes correspond to Power-pill abilities that affect multiple Sprites and have limited
+ * duration.
+ */
 public enum PacmanEventType {
-    POWER_PILL_ACTIVATED,
-    POWER_PILL_DEACTIVATED
+  //    POWER_PILL_ACTIVATED,
+//    POWER_PILL_DEACTIVATED,
+  SPEED_UP_ACTIVATED,
+  SPEED_UP_DEACTIVATED,
+  GHOST_SLOWDOWN_ACTIVATED,
+  GHOST_SLOWDOWN_DEACTIVATED,
+  FRIGHTEN_ACTIVATED,
+  FRIGHTEN_DEACTIVATED,
+  POINT_BONUS_ACTIVATED,
+  POINT_BONUS_DEACTIVATED,
 }
