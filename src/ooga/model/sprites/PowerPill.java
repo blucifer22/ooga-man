e67023ffade515eb58e09595761ca2144b70ffc4
Matthew Belissary <mab185@duke.edu>
2021-04-12 18:40:58 -0400

    added powerpill (not powers yet)

--- src/ooga/model/sprites/PowerPill.java
+++ src/ooga/model/sprites/PowerPill.java
@@ -1,5 +1,43 @@
 package ooga.model.sprites;
 
-public class PowerPill {
+import ooga.model.PacmanGameState;
+import ooga.model.SpriteCoordinates;
+import ooga.util.Vec2;
 
+/**
+ * Basic Dot, consumable by Pac-Man to increase the score.
+ *
+ * @author Matthew Belissary
+ */
+public class PowerPill extends Sprite {
+
+  public PowerPill(SpriteCoordinates position, Vec2 direction) {
+    super(position, direction);
+  }
+
+  @Override
+  public String getType() {
+    return "powerpill";
+  }
+
+  @Override
+  public void uponHitBy(Sprite other, PacmanGameState state) {
+    state.prepareRemove(this);
+    System.out.println("Powerup Consumed");
+  }
+
+  @Override
+  public void step(double dt, PacmanGameState pacmanGameState) {
+
+  }
+
+  @Override
+  public boolean mustBeConsumed() {
+    return false;
+  }
+
+  @Override
+  public boolean isDeadlyToPacMan() {
+    return false;
+  }
 }
