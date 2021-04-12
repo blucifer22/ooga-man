d7f3c2f0ae42f9d8bcada645704432726ef4832a
David Coffman <david.coffman@duke.edu>
2021-04-12 18:09:37 -0400

    Add button and text label to main menu

--- src/ooga/view/views/MenuView.java
+++ src/ooga/view/views/MenuView.java
@@ -1,12 +1,12 @@
 package ooga.view.views;
 
-import javafx.beans.property.ReadOnlyStringProperty;
+import javafx.geometry.HPos;
 import javafx.geometry.Pos;
-import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 import ooga.view.internal_api.View;
 import ooga.view.language.api.LanguageService;
 import ooga.view.theme.api.ThemeService;
@@ -21,16 +21,24 @@ public class MenuView implements View {
     this.themeService = themeService;
     this.languageService = languageService;
     this.primaryView = new GridPane();
+    this.primaryView.setGridLinesVisible(true);
     this.primaryView.setAlignment(Pos.CENTER);
 
     buildScene();
   }
 
   private void buildScene() {
-    Text title = new Text();
+    Label title = new Label();
     title.setFont(Font.font("Monospaced", 30));
     title.textProperty().bind(languageService.getLocalizedString("pacman"));
+    GridPane.setHalignment(title, HPos.CENTER);
     this.primaryView.add(title, 1, 1);
+
+    Button start = new Button();
+    start.textProperty().bind(languageService.getLocalizedString("pacman"));
+    start.setFont(Font.font("Monospaced", 30));
+    GridPane.setHalignment(start, HPos.CENTER);
+    this.primaryView.add(start, 1, 2);
   }
 
   @Override
