73c3049b3f43d5459cd14f02dff624b5c784277f
Marc Chmielewski <msc68@duke.edu>
2021-04-13 14:43:04 -0400

    Removed redundant imports from Controller

--- src/ooga/controller/Controller.java
+++ src/ooga/controller/Controller.java
@@ -7,31 +7,21 @@ import javafx.util.Duration;
 import ooga.model.GhostAI;
 import ooga.model.PacmanGameState;
 import ooga.model.SpriteCoordinates;
-import ooga.model.TileCoordinates;
-import ooga.model.api.ObservableGrid;
-import ooga.model.api.ObservableSprite;
-import ooga.model.api.ObservableTile;
-import ooga.model.api.SpriteEvent.EventType;
-import ooga.model.api.SpriteObserver;
-import ooga.model.api.TileEvent;
-import ooga.model.api.TileObserver;
 import ooga.model.leveldescription.JSONDescriptionFactory;
 import ooga.model.sprites.Blinky;
 import ooga.model.sprites.Clyde;
-import ooga.model.sprites.Ghost;
 import ooga.model.sprites.Dot;
 import ooga.model.sprites.Inky;
 import ooga.model.sprites.PacMan;
 import ooga.model.sprites.Pinky;
 import ooga.model.sprites.PowerPill;
-import ooga.model.sprites.Sprite;
 import ooga.util.Vec2;
 import ooga.view.UIController;
 import ooga.view.views.GameView;
 
 public class Controller implements GameStateController {
 
-  private static final double TIMESTEP = 1.0/60.0;
+  private static final double TIMESTEP = 1.0 / 60.0;
   private final UIController uiController;
   private final HumanInputManager inputManager;
   private GhostAI blinkyAI;
@@ -43,7 +33,7 @@ public class Controller implements GameStateController {
     this.inputManager = new HumanInputManager();
     this.uiController = new UIController(primaryStage, this, this.inputManager);
     uiController.showMenu();
-    //startGame();
+    // startGame();
   }
 
   public void startGame() {
@@ -54,17 +44,19 @@ public class Controller implements GameStateController {
     pgs.addGridRebuildObserver(gv.getGridRebuildObserver());
 
     try {
-      pgs.loadGrid(new JSONDescriptionFactory().getGridDescriptionFromJSON("data/levels/grids/demo_grid.json"));
-    } catch(Exception e) {
+      pgs.loadGrid(
+          new JSONDescriptionFactory()
+              .getGridDescriptionFromJSON("data/levels/grids/demo_grid.json"));
+    } catch (Exception e) {
 
     }
 
-    PacMan pacman = new PacMan(new SpriteCoordinates(new Vec2(1.5, 1.5)), new Vec2(0,0), 5.0);
+    PacMan pacman = new PacMan(new SpriteCoordinates(new Vec2(1.5, 1.5)), new Vec2(0, 0), 5.0);
 
-    Blinky blinky = new Blinky(new SpriteCoordinates(new Vec2(11.5, 11.5)), new Vec2(0,0), 5.0);
-    Inky inky = new Inky(new SpriteCoordinates(new Vec2(11.5, 11.5)), new Vec2(0,0), 5.0);
-    Pinky pinky = new Pinky(new SpriteCoordinates(new Vec2(11.5, 11.5)), new Vec2(0,0), 5.0);
-    Clyde clyde = new Clyde(new SpriteCoordinates(new Vec2(11.5, 11.5)), new Vec2(0,0), 5.0);
+    Blinky blinky = new Blinky(new SpriteCoordinates(new Vec2(11.5, 11.5)), new Vec2(0, 0), 5.0);
+    Inky inky = new Inky(new SpriteCoordinates(new Vec2(11.5, 11.5)), new Vec2(0, 0), 5.0);
+    Pinky pinky = new Pinky(new SpriteCoordinates(new Vec2(11.5, 11.5)), new Vec2(0, 0), 5.0);
+    Clyde clyde = new Clyde(new SpriteCoordinates(new Vec2(11.5, 11.5)), new Vec2(0, 0), 5.0);
 
     Dot dot1 = new Dot(new SpriteCoordinates(new Vec2(4.5, 4.5)), new Vec2(0, 0));
     Dot dot2 = new Dot(new SpriteCoordinates(new Vec2(1.5, 3.5)), new Vec2(0, 0));
@@ -95,8 +87,8 @@ public class Controller implements GameStateController {
 
     uiController.showGameView();
 
-//    pgs.step(TIMESTEP);
-//    pgs.step(TIMESTEP);
+    //    pgs.step(TIMESTEP);
+    //    pgs.step(TIMESTEP);
 
     KeyFrame frame = new KeyFrame(Duration.seconds(TIMESTEP), e -> pgs.step(TIMESTEP)); //
     // TODO: remove grid from step parameter
