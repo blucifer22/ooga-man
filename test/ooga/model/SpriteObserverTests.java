01d9f510e5792d7a806cbd4e0a439884bbc17538
Franklin Wei <franklin@rockbox.org>
2021-04-13 23:44:51 -0400

    Update SpriteObserverTest for animation.

--- test/ooga/model/SpriteObserverTests.java
+++ test/ooga/model/SpriteObserverTests.java
@@ -6,6 +6,7 @@ import ooga.model.api.SpriteEvent;
 import ooga.model.api.SpriteEvent.EventType;
 import ooga.model.api.SpriteObserver;
 import ooga.model.sprites.Sprite;
+import ooga.model.sprites.animation.StillAnimation;
 import ooga.util.Vec2;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -83,9 +84,8 @@ class TestObservableSprite extends Sprite {
 
   public static final String SPRITE_TYPE = "TEST_SPRITE";
 
-  @Override
-  public String getCostume() {
-    return SPRITE_TYPE;
+  public TestObservableSprite() {
+    super(new StillAnimation(SPRITE_TYPE));
   }
 
   @Override
