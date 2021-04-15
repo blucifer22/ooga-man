e7fefd13cade1184dc949ae5ef18373ce6f69d87
Franklin Wei <franklin@rockbox.org>
2021-04-14 21:59:02 -0400

    clyde: update for animation.

--- src/ooga/model/sprites/Clyde.java
+++ src/ooga/model/sprites/Clyde.java
@@ -8,10 +8,8 @@ import ooga.util.Vec2;
 /** @author Matthew Belissary */
 public class Clyde extends Ghost{
 
-  public static final String TYPE = "clyde";
-
   public Clyde(SpriteCoordinates position, Vec2 direction, double speed) {
-    super(new StillAnimation("clyde"),
+    super("clyde",
             position, direction, speed);
   }
 
