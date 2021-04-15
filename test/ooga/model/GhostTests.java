c1178316eb2e3c4d242eeb5b784b5e62c89ff303
Marc Chmielewski <msc68@duke.edu>
2021-04-15 12:14:21 -0400

    Completed Ghost SpriteDescription constructor tests

--- test/ooga/model/GhostTests.java
+++ test/ooga/model/GhostTests.java
@@ -4,8 +4,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import ooga.model.leveldescription.SpriteDescription;
 import ooga.model.sprites.Blinky;
+import ooga.model.sprites.Clyde;
 import ooga.model.sprites.Ghost;
 import ooga.model.sprites.Ghost.GhostBehavior;
+import ooga.model.sprites.Inky;
+import ooga.model.sprites.Pinky;
 import ooga.util.Vec2;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -17,23 +20,69 @@ import org.junit.jupiter.api.Test;
  */
 public class GhostTests {
 
-  SpriteDescription defaultSpriteDescription;
+  SpriteDescription blinkySpriteDescription;
+  SpriteDescription inkySpriteDescription;
+  SpriteDescription pinkySpriteDescription;
+  SpriteDescription clydeSpriteDescription;
 
   @BeforeEach
   public void init() {
-    String spriteClassName = "Blinky";
+
+    // Create SpriteDescriptions for each Ghost type
+    blinkySpriteDescription = createDefaultGhostDescription("Blinky");
+    inkySpriteDescription = createDefaultGhostDescription("Inky");
+    pinkySpriteDescription = createDefaultGhostDescription("Pinky");
+    clydeSpriteDescription = createDefaultGhostDescription("Clyde");
+  }
+
+  private SpriteDescription createDefaultGhostDescription(
+      String spriteClassName) {
     String inputSource = "GHOST_AI";
     SpriteCoordinates startingCoordinates = new SpriteCoordinates(new Vec2(5, 5));
-    defaultSpriteDescription =
-        new SpriteDescription(spriteClassName, inputSource, startingCoordinates);
+    return new SpriteDescription(spriteClassName, inputSource, startingCoordinates);
   }
 
   @Test
   public void testBlinkySpriteDescriptionConstructor() {
-    Ghost blinky = new Blinky(defaultSpriteDescription);
+    Ghost blinky = new Blinky(blinkySpriteDescription);
+
     // Assert that Blinky starts by WAITing in the correct location!
+    assertEquals(blinky.getCurrentAnimation().getCurrentCostume(), "blinky_right_1");
     assertEquals(blinky.getCoordinates().getPosition().getX(), 5);
     assertEquals(blinky.getCoordinates().getPosition().getY(), 5);
     assertEquals(blinky.getGhostBehavior(), GhostBehavior.WAIT);
+  }
+
+  @Test
+  public void testInkySpriteDescriptionConstructor() {
+    Ghost inky = new Inky(inkySpriteDescription);
+
+    // Assert that Inky starts by WAITing in the correct location!
+    assertEquals(inky.getCurrentAnimation().getCurrentCostume(), "inky_right_1");
+    assertEquals(inky.getCoordinates().getPosition().getX(), 5);
+    assertEquals(inky.getCoordinates().getPosition().getY(), 5);
+    assertEquals(inky.getGhostBehavior(), GhostBehavior.WAIT);
+  }
+
+  @Test
+  public void testPinkySpriteDescriptionConstructor() {
+    Ghost pinky = new Pinky(pinkySpriteDescription);
+
+    // Assert that Pinky starts by WAITing in the correct location!
+    assertEquals(pinky.getCurrentAnimation().getCurrentCostume(), "pinky_right_1");
+    assertEquals(pinky.getCoordinates().getPosition().getX(), 5);
+    assertEquals(pinky.getCoordinates().getPosition().getY(), 5);
+    assertEquals(pinky.getGhostBehavior(), GhostBehavior.WAIT);
+  }
+
+  @Test
+  public void testClydeSpriteDescriptionConstructor() {
+    Ghost clyde = new Clyde(clydeSpriteDescription);
+
+    // Assert that Pinky starts by WAITing in the correct location!
+    assertEquals(clyde.getCurrentAnimation().getCurrentCostume(), "clyde_right_1");
+    assertEquals(clyde.getCoordinates().getPosition().getX(), 5);
+    assertEquals(clyde.getCoordinates().getPosition().getY(), 5);
+    assertEquals(clyde.getGhostBehavior(), GhostBehavior.WAIT);
   }
 }
