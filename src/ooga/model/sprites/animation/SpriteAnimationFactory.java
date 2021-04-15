126eccd39d1f16531605d014f8db01ac652b77ef
Marc Chmielewski <msc68@duke.edu>
2021-04-14 20:43:24 -0400

    Midway through implementation of SpriteAnimationFactory

--- src/ooga/model/sprites/animation/SpriteAnimationFactory.java
+++ src/ooga/model/sprites/animation/SpriteAnimationFactory.java
@@ -5,16 +5,23 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import ooga.model.sprites.animation.PeriodicAnimation.FrameOrder;
 
+/**
+ * This class is a relatively simple factory that creates sprite animations for all of the non-still
+ * types of sprite in the Pac-Man variations.
+ *
+ * @author Marc Chmielewski
+ * @author Franklin Wei
+ */
 public class SpriteAnimationFactory {
-  private String spriteName;
+  private final String spriteName;
 
   public SpriteAnimationFactory(String spriteName) {
     this.spriteName = spriteName;
   }
 
-  public ObservableAnimation createAnimation(String animationType) throws IllegalArgumentException {
-    List<String> costumes =
-    return new FreeRunningPeriodicAnimation();
+  public ObservableAnimation createAnimation(SpriteAnimationType type)
+      throws IllegalArgumentException {
+    return type.getAnimation(spriteName);
   }
 
   public enum SpriteAnimationType {
@@ -25,20 +32,25 @@ public class SpriteAnimationFactory {
     GHOST_FRIGHTENED(false, "frightened", 2),
     GHOST_FRIGHTENED_END(false, "frightened_end", 2),
     GHOST_UP_EYES(false, "eyes_up", 1),
-    GHOST_DOWN_EYES(false, "eyes_down",1),
-    GHOST_LEFT_EYES(false, "eyes_left",1),
+    GHOST_DOWN_EYES(false, "eyes_down", 1),
+    GHOST_LEFT_EYES(false, "eyes_left", 1),
     GHOST_RIGHT_EYES(false, "eyes_right", 1),
-    PACMAN_CHOMP(true, "chomp", 3, 1.0/6.0, FrameOrder.TRIANGLE);
+    PACMAN_CHOMP(true, "chomp", 3, 1.0 / 6.0, FrameOrder.TRIANGLE),
+    POWER_PILL_BLINK(true, "blink", 2, 1.0/6.0, FrameOrder.SAWTOOTH);
 
-    private static final double DEFAULT_FRAME_PERIOD = 1.0/6.0;
-    private boolean spriteSpecific;
-    private String costumeBaseName;
-    private int frameCount;
-    private double framePeriod;
-    private FrameOrder frameOrder;
+    private static final double DEFAULT_FRAME_PERIOD = 1.0 / 15.0;
+    private final boolean spriteSpecific;
+    private final String costumeBaseName;
+    private final int frameCount;
+    private final double framePeriod;
+    private final FrameOrder frameOrder;
 
-    SpriteAnimationType(boolean spriteSpecific, String costumeBaseName, int frameCount,
-        double framePeriod, FrameOrder frameOrder) {
+    SpriteAnimationType(
+        boolean spriteSpecific,
+        String costumeBaseName,
+        int frameCount,
+        double framePeriod,
+        FrameOrder frameOrder) {
       this.spriteSpecific = spriteSpecific;
       this.costumeBaseName = costumeBaseName;
       this.frameCount = frameCount;
@@ -51,11 +63,12 @@ public class SpriteAnimationFactory {
     }
 
     public ObservableAnimation getAnimation(String spriteName) {
-      List<String> animationCostumes = IntStream.range(1, frameCount + 1)
-          .mapToObj(num -> ((spriteSpecific ? spriteName + "_" : "") + costumeBaseName + "_" + num))
-          .collect(Collectors.toList());
-      return new FreeRunningPeriodicAnimation(animationCostumes, )
+      List<String> animationCostumes =
+          IntStream.range(1, frameCount + 1)
+              .mapToObj(
+                  num -> ((spriteSpecific ? spriteName + "_" : "") + costumeBaseName + "_" + num))
+              .collect(Collectors.toList());
+      return new FreeRunningPeriodicAnimation(animationCostumes, frameOrder, framePeriod);
     }
   }
-
 }
