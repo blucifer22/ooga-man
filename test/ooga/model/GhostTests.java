8dcd4653b7a28352367590f8949aea6cbf1e771a
Marc Chmielewski <msc68@duke.edu>
2021-04-15 11:56:31 -0400

    Starting Ghost test suite

---
+++ test/ooga/model/GhostTests.java
@@ -0,0 +1,21 @@
+package ooga.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * A simple test suite for the Ghost class.
+ *
+ * @author Marc Chmielewski
+ */
+public class GhostTests {
+  @BeforeEach
+  public void init() {
+
+  }
+
+  @Test
+  public void testGhostConstructor() {
+
+  }
+}
