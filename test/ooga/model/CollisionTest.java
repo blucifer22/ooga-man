2f47e94447a6e8d9f0f48c5aa4114476bea72d1e
George Hong <grh17@duke.edu>
2021-04-19 19:13:47 -0600

    Add tests to timer for new reset and clear methods
    Also refactored tests that spoofed user input into a separate class to
    reduce repeated code.

--- test/ooga/model/CollisionTest.java
+++ test/ooga/model/CollisionTest.java
@@ -55,39 +55,13 @@ public class CollisionTest {
   @Test
   public void collisionWithDotIncrementsScore() {
 
-    class TestInputSource implements InputSource {
+    List<Vec2> prepopulatedActions = new ArrayList<>();
 
-      private final List<Vec2> prepopulatedActions = new ArrayList<>();
-      private int dex = 0;
-
-      public TestInputSource() {
-        for (int j = 0; j < 1000; j++) {
-          prepopulatedActions.add(new Vec2(0, -1));
-        }
-      }
-
-      @Override
-      public Vec2 getRequestedDirection() {
-        return prepopulatedActions.get(dex++);
-      }
-
-      @Override
-      public boolean isActionPressed() {
-        return false;
-      }
-
-      /**
-       * Adds a Sprite target to the InputSource.
-       *
-       * @param target The Sprite to add to the InputSource.
-       */
-      @Override
-      public void addTarget(Sprite target) {
-
-      }
+    for (int j = 0; j < 1000; j++) {
+      prepopulatedActions.add(new Vec2(0, -1));
     }
-    TestInputSource input = new TestInputSource() {
-    };
+    SeededTestInputSource input = new SeededTestInputSource();
+    input.addActions(prepopulatedActions);
     pacMan.setInputSource(input);
 
     for (int k = 0; k < 1000; k++) {
@@ -101,43 +75,19 @@ public class CollisionTest {
   @Test
   public void teleporterBasicTest() {
 
-    class TestInputSource implements InputSource {
+    List<Vec2> prepopulatedActions = new ArrayList<>();
 
-      private final List<Vec2> prepopulatedActions = new ArrayList<>();
-      private int dex = 0;
-
-      public TestInputSource() {
-        for (int j = 0; j < 10; j++) {
-          prepopulatedActions.add(new Vec2(-1, 0));
-        }
-      }
-
-      @Override
-      public Vec2 getRequestedDirection() {
-        return prepopulatedActions.get(dex++);
-      }
-
-      @Override
-      public boolean isActionPressed() {
-        return false;
-      }
-
-      /**
-       * Adds a Sprite target to the InputSource.
-       *
-       * @param target The Sprite to add to the InputSource.
-       */
-      @Override
-      public void addTarget(Sprite target) {
-
-      }
+    for (int j = 0; j < 10; j++) {
+      prepopulatedActions.add(new Vec2(-1, 0));
     }
+    SeededTestInputSource input = new SeededTestInputSource();
+    input.addActions(prepopulatedActions);
 
     Vec2 position = new Vec2(2.5, 2.5);
     Vec2 direction = new Vec2(-1, 0);
     SpriteCoordinates spriteCoordinates = new SpriteCoordinates(position);
     pacMan = new PacMan(spriteCoordinates, direction, 5);
-    pacMan.setInputSource(new TestInputSource());
+    pacMan.setInputSource(input);
 
     state = new PacmanGameState();
     state.loadGrid(grid);
@@ -152,8 +102,8 @@ public class CollisionTest {
     state.addSprite(teleporter1);
     state.addSprite(teleporter2);
 
-    for (int k = 0; k < 6; k++){
-      state.step(1/60.);
+    for (int k = 0; k < 6; k++) {
+      state.step(1 / 60.);
     }
     assertTrue(pacMan.getCoordinates().getPosition().getX() > 2.5);
   }
---
+++ test/ooga/model/SeededTestInputSource.java
@@ -0,0 +1,34 @@
+package ooga.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import ooga.model.sprites.Sprite;
+import ooga.util.Vec2;
+
+/**
+ * Test input source that plays a set number of predetermined actions.
+ */
+public class SeededTestInputSource implements InputSource {
+
+  private final List<Vec2> prepopulatedActions = new ArrayList<>();
+  private int dex = 0;
+
+  public void addActions(List<Vec2> actions) {
+    prepopulatedActions.addAll(actions);
+  }
+
+  @Override
+  public Vec2 getRequestedDirection() {
+    return prepopulatedActions.get(dex++);
+  }
+
+  @Override
+  public boolean isActionPressed() {
+    return false;
+  }
+
+  @Override
+  public void addTarget(Sprite target) {
+
+  }
+}
