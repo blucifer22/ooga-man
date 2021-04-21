b1233522796cf67515b3619a1f9a69cca4fd7ad5
George Hong <grh17@duke.edu>
2021-04-21 13:44:31 -0600

    Refactor update Ghost movement speed on level transition
    All Sprites now offer a method to change Sprite behavior as a function
    of current level, allowing Pac-Man to also speed up if desired or to
    change cherry respawn times.

--- src/ooga/model/PacmanGameState.java
+++ src/ooga/model/PacmanGameState.java
@@ -88,6 +88,8 @@ public class PacmanGameState
     clock.reset();
   }
 
+
+
   public void loadPacmanLevel(PacmanLevel level) {
     for (Sprite sprite : level.getSprites()) {
       addSprite(sprite);
@@ -145,8 +147,9 @@ public class PacmanGameState
     for (Sprite sprite : level.getSprites()) {
       if (sprite.getSwapClass() == SwapClass.GHOST){
         // TODO: Change to not downcast
-        MoveableSprite mover = (MoveableSprite) sprite;
-        mover.setMovementSpeed(Math.min(mover.getMovementSpeed() + (0.5 * roundNumber), 6));
+//        MoveableSprite mover = (MoveableSprite) sprite;
+//        mover.setMovementSpeed(Math.min(mover.getMovementSpeed() + (0.5 * roundNumber), 6));
+        sprite.adjustSpritePropertyWithLevel(roundNumber);
       }
       addSprite(sprite);
     }
@@ -157,10 +160,6 @@ public class PacmanGameState
 
   protected void incrementLevel() {
     roundNumber++;
-  }
-
-  protected int getRoundNumber() {
-    return roundNumber;
   }
 
   protected boolean isPacmanConsumed() {
@@ -250,6 +249,11 @@ public class PacmanGameState
   @Override
   public int getPacmanLivesRemaining() {
     return pacmanLivesRemaining;
+  }
+
+  @Override
+  public int getRoundNumber() {
+    return roundNumber;
   }
 
   public void isPacmanDead(boolean isPacmanDead) {
