25be093f927cd7552d28ff4bfdbb6158581f0b6d
George Hong <grh17@duke.edu>
2021-04-21 20:16:23 -0600

    Add InkyAI

---
+++ src/ooga/model/ai/InkyAI.java
@@ -0,0 +1,29 @@
+package ooga.model.ai;
+
+import ooga.model.PacmanGrid;
+import ooga.model.sprites.Sprite;
+import ooga.util.Vec2;
+
+public class InkyAI extends GhostAI {
+
+  public InkyAI(PacmanGrid grid, Sprite ghost) {
+    super(grid, ghost);
+  }
+
+  /**
+   * Inky AI's chase behavior is based on its proximity to Pac-Man.  If Pac-Man is within 8 tiles of
+   * Inky, Inky aggressively chases after Pac-Man. Otherwise, Inky wanders in scatter mode.
+   *
+   * @return direction to move the ghost
+   */
+  @Override
+  protected Vec2 chaseBehavior() {
+    Vec2 targetTilePos = getTarget().getCoordinates().getTileCoordinates().toVec2();
+    Vec2 currentTilePos = getGhost().getCoordinates().getTileCoordinates().toVec2();
+    if (currentTilePos.distance(targetTilePos) < 8) {
+      return reduceDistance(targetTilePos, currentTilePos);
+    } else {
+      return scatterBehavior();
+    }
+  }
+}
