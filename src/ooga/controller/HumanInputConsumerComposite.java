ae7e81ffb702993b1af9bf766537290bf38b3152
David Coffman <david.coffman@duke.edu>
2021-04-22 21:19:10 -0400

    Add HumanInputConsumerComposite for all of our multi-input needs

---
+++ src/ooga/controller/HumanInputConsumerComposite.java
@@ -0,0 +1,43 @@
+package ooga.controller;
+
+import java.util.HashSet;
+import javafx.scene.input.KeyCode;
+import ooga.view.io.HumanInputConsumer;
+
+public class HumanInputConsumerComposite implements HumanInputConsumer {
+
+  private final HashSet<HumanInputConsumer> components = new HashSet<>();
+
+  public void addConsumer(HumanInputConsumer consumer) {
+    components.add(consumer);
+  }
+
+  public void removeConsumer(HumanInputConsumer consumer) {
+    components.remove(consumer);
+  }
+
+  /**
+   * This method handles the behavior associated with depressed keys and cues the calling class to
+   * spin up an appropriate response.
+   *
+   * @param keyCode The KeyCode of the currently depressed key.
+   */
+  @Override
+  public void onKeyPress(KeyCode keyCode) {
+    for (HumanInputConsumer consumer: components) {
+      consumer.onKeyPress(keyCode);
+    }
+  }
+
+  /**
+   * This method handles the behavior associated with the release of a key
+   *
+   * @param keyCode The KeyCode of the key that is being released.
+   */
+  @Override
+  public void onKeyRelease(KeyCode keyCode) {
+    for (HumanInputConsumer consumer: components) {
+      consumer.onKeyRelease(keyCode);
+    }
+  }
+}
