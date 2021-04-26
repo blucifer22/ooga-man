1b83f8809fedeffc4e52169bf8f1b05e29951ea6
Franklin Wei <franklin@rockbox.org>
2021-04-25 23:55:30 -0400

    Add "Ready!" sprite to classic game.

--- src/ooga/model/sprites/animation/SpriteAnimationFactory.java
+++ src/ooga/model/sprites/animation/SpriteAnimationFactory.java
@@ -49,6 +49,7 @@ public class SpriteAnimationFactory {
     GAME_OVER_FLASH(false, "game_over", 2, 1.0 / 4.0, FrameOrder.SAWTOOTH),
     PACMAN_WIN_FLASH(false, "pacman_win", 2, 1.0 / 4.0, FrameOrder.SAWTOOTH),
     GHOST_WIN_FLASH(false, "ghosts_win", 2, 1.0 / 4.0, FrameOrder.SAWTOOTH),
+    READY(false, "ready", 1),
     BLANK(false, "blank", 1);
 
     private static final double DEFAULT_FRAME_PERIOD = 1.0 / 8.0;
