67697af6d98104ff1073604fdc688199240b0563
Marc Chmielewski <msc68@duke.edu>
2021-04-26 16:39:02 -0400

    Updated docs for LevelBuilder

--- src/ooga/model/leveldescription/LevelBuilder.java
+++ src/ooga/model/leveldescription/LevelBuilder.java
@@ -20,7 +20,8 @@ import ooga.model.sprites.SwapClass;
 
 /**
  * The StageBuilder is a stripped-down, non-steppable snapshot of the starting Pac-Man game state.
- * This allows for similar rendering.
+ * This allows for similar rendering, and ultimately serialization into a application-parsable
+ * JSON file that describes a PacmanLevel.
  *
  * @author George Hong
  * @author Marc Chmielewski
@@ -36,6 +37,11 @@ public class LevelBuilder implements SpriteExistenceObservable, GridRebuildObser
   private int pacmanCount;
   private BuilderState currentState;
 
+  /**
+   * This constructor is the general constructor for this LevelBuilder. It takes no arguments, but
+   * handles the instantiation of all of the containers within this LevelBuilder. It also instantiates
+   * the currentState of this LevelBuilder to TILING, and the initial pacmanCount to 0.
+   */
   public LevelBuilder() {
     spriteExistenceObservers = new HashSet<>();
     gridRebuildObservers = new HashSet<>();
@@ -46,15 +52,29 @@ public class LevelBuilder implements SpriteExistenceObservable, GridRebuildObser
     pacmanCount = 0;
   }
 
+  /**
+   * This method returns the current BuilderState of this LevelBuilder.
+   *
+   * Implemented as part of the LevelEditor interface.
+   *
+   * @return The current state of this LevelBuilder
+   */
   @Override
   public BuilderState getBuilderState() {
     return currentState;
   }
 
+  /**
+   * This method advances the current BuilderState of this LevelBuilder from TILING to SPRITE_PLACEMENT
+   * if it makes logical sense to do so. Else, it does nothing.
+   *
+   * Implemented as part of the LevelEditor interface.
+   *
+   */
   @Override
   public void advanceState() {
-    switch (currentState) {
-      case TILING -> currentState = BuilderState.SPRITE_PLACEMENT;
+    if (currentState == BuilderState.TILING) {
+      currentState = BuilderState.SPRITE_PLACEMENT;
     }
   }
 
@@ -62,8 +82,13 @@ public class LevelBuilder implements SpriteExistenceObservable, GridRebuildObser
    * Select is a modal method that changes the functionality of a click depending on the state of
    * the level builder.  To change what select does, advance to the next state.
    *
-   * @param x
-   * @param y
+   * <p>If this LevelBuilder is currently in TILING mode, select calls pokeTile on the Tile at the
+   * provided X/Y coordinates</p>
+   *<p>If this LevelBuilder is currently in SPRITE_PLACEMENT mode, select calls addSprite and adds
+   *  the currently selected Sprite on the palette to the Tile at the provided X/Y coordinates</p>
+   *
+   * @param x The X-coordinate of the selected Tile
+   * @param y The Y-coordinate of the selected Tile
    */
   public void select(int x, int y) {
     switch (currentState) {
@@ -72,15 +97,39 @@ public class LevelBuilder implements SpriteExistenceObservable, GridRebuildObser
     }
   }
 
+  /**
+   * This method returns this LevelBuilder's palette.
+   *
+   * Implemented as part of the LevelEditor interface.
+   *
+   * @return This LevelBuilder's palette.
+   */
   @Override
   public Palette getPalette() {
     return palette;
   }
 
+  /**
+   * This method returns the PacmanLevel that is currently associated with this LevelBuilder.
+   * This is currently just used for testing, but was made public to facilitate possible extensions
+   * in dynamic serialization.
+   *
+   * @return The PacmanLevel that is currently associated with this LevelBuilder.
+   */
   public PacmanLevel getLevel() {
     return level;
   }
 
+  /**
+   * This method writes the PacmanLevel, level, that is currently held by this LevelBuilder to JSON
+   * by crafting a LevelDescription fom it, setting the gameMode to "CLASSIC" and then leveraging
+   * the existing levelDescription.toJSON() method to facilitate the actual serialization.
+   *
+   * Implemented as part of the LevelEditor interface.
+   *
+   * @param file The input JSON file to write the serialization to.
+   * @throws IOException When provided with an invalid JSON File. (Bad path, etc.)
+   */
   @Override
   public void writeToJSON(File file) throws IOException {
     LevelDescription levelDescription = new LevelDescription(level);
@@ -88,6 +137,18 @@ public class LevelBuilder implements SpriteExistenceObservable, GridRebuildObser
     levelDescription.toJSON(file.getPath());
   }
 
+  /**
+   * This method adds a Sprite to the grid Tile that has been clicked on. The type of Sprite is
+   * determined by what type of Sprite is currently selected by the Palette. In the event that the
+   * user is trying to place Pac-Man on the grid, this method performs a simple check to ensure that
+   * there hasn't already been a Pac-Man placed on the grid, and throws an IllegalStateException if
+   * that is not the case.
+   *
+   * Implemented as part of the LevelEditor interface.
+   *
+   * @param x x-coordinate of grid Tile to add Sprite to
+   * @param y y-coordinate of grid Tile to add Sprite to
+   */
   @Override
   public void addSprite(int x, int y) {
     double xCenter = x + 0.5;
@@ -104,6 +165,17 @@ public class LevelBuilder implements SpriteExistenceObservable, GridRebuildObser
     }
   }
 
+  /**
+   * This method changes the state of the grid Tile that has been clicked by the user of the
+   * program. The coordinates of this tile are passed in as parameters, which are then processed
+   * by the method to determine the specific tile that should have its state advanced, and then
+   * notifies the GridRebuildObservers to update this state visually.
+   *
+   * Implemented as part of the LevelEditor interface.
+   *
+   * @param x x-coordinate of grid to change tile type
+   * @param y y-coordinate of grid to change tile type
+   */
   @Override
   public void pokeTile(int x, int y) {
     Tile tile = level.getGrid().getTile(new TileCoordinates(x, y));
@@ -129,6 +201,19 @@ public class LevelBuilder implements SpriteExistenceObservable, GridRebuildObser
     return tile;
   }
 
+  /**
+   * This method clears all of the Sprites that currently reside on a particular grid Tile. As with
+   * other methods in this class, this Tile is determined from the provided coordinates.
+   * This method also performs a check to verify that this LevelBuilder is in the SPRITE_PLACEMENT
+   * state and throws an IllegalStateException if this condition is not upheld. As would be expected,
+   * this method also maintains the count of Pac-Man instances and handles the notification of the
+   * attached SpriteExistanceObservers.
+   *
+   * Implemented as part of the LevelEditor interface.
+   *
+   * @param x x-coordinate of grid to remove all Sprites from
+   * @param y y-coordinate of grid to remove all Sprites from
+   */
   @Override
   public void clearSpritesOnTile(int x, int y) {
     if (currentState != BuilderState.SPRITE_PLACEMENT) {
@@ -177,11 +262,28 @@ public class LevelBuilder implements SpriteExistenceObservable, GridRebuildObser
   }
 
 
+  /**
+   * This method attaches a new GridRebuildObserver to this LevelBuilder, thus allowing the front-end
+   * to be notified of any changes to the grid state.
+   *
+   * Implemented as part of the GridRebuildObservable interface.
+   *
+   * @param observer The GridRebuildObserver to be added to this LevelBuilder's set of observers.
+   */
   @Override
   public void addGridRebuildObserver(GridRebuildObserver observer) {
     gridRebuildObservers.add(observer);
   }
 
+  /**
+   * This method attaches a new SpriteExistenceObserver to this LevelBuilder, thus allowing
+   * the front-end to be notified of any changes to the Sprites held by this LevelBuilder.
+   *
+   * Implemented as part of the SpriteExistenceObservable interface.
+   *
+   * @param spriteExistenceObserver The SpriteExistenceObserver to be added to
+   *                               this LevelBuilder's set of observers.
+   */
   @Override
   public void addSpriteExistenceObserver(SpriteExistenceObserver spriteExistenceObserver) {
     spriteExistenceObservers.add(spriteExistenceObserver);
@@ -205,6 +307,14 @@ public class LevelBuilder implements SpriteExistenceObservable, GridRebuildObser
     }
   }
 
+  /**
+   * This enum enumerates the two possible BuilderStates that a given LevelBuilder can be in.
+   *
+   * TILING: The state in which TILIING modifications can be made to the PacmanLevel
+   * in the LevelBuilder
+   * SPRITE_PLACEMENT: The state in which SPRITE_PLACEMENT modifications can be made to the
+   * PacmanLevel in the LevelBuilder
+   */
   public enum BuilderState {
     TILING,
     SPRITE_PLACEMENT,
