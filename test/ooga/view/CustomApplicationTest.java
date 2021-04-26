4c4e80456d28ac2d5c118a3c57325e7359521b6f
David Coffman <david.coffman@duke.edu>
2021-04-25 23:28:07 -0400

    Fix CustomApplicationTest

--- test/ooga/view/CustomApplicationTest.java
+++ test/ooga/view/CustomApplicationTest.java
@@ -1,6 +1,9 @@
 package ooga.view;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicBoolean;
 import javafx.application.Platform;
 import org.testfx.framework.junit5.ApplicationTest;
 
@@ -9,14 +12,21 @@ public class CustomApplicationTest extends ApplicationTest {
 
   protected void syncFXRun(Runnable r) throws InterruptedException {
     CountDownLatch cdl = new CountDownLatch(1);
+    AtomicBoolean fail = new AtomicBoolean(false);
     Platform.runLater(() -> {
       try {
         r.run();
       } catch (Exception e) {
         e.printStackTrace();
+      } catch (Error e) {
+        e.printStackTrace();
+        fail.set(true);
       }
       cdl.countDown();
     });
     cdl.await();
+    if (fail.get()) {
+      fail();
+    }
   }
 }
