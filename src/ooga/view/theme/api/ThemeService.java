ef0a909f6ab0738f1e266db5d0418821fe3b4f14
David Coffman <david.coffman@duke.edu>
2021-04-27 10:35:50 -0400

    Add ThemeService docs

--- src/ooga/view/theme/api/ThemeService.java
+++ src/ooga/view/theme/api/ThemeService.java
@@ -1,8 +1,25 @@
 package ooga.view.theme.api;
 
+/**
+ * The view-facing interface of a theme management class. Allows for {@link ThemedObject}s to
+ * register for theme-change updates, as well as to query for the current {@link Theme}
+ * (typically in response to updates received following the former).
+ *
+ * @author David Coffman
+ */
 public interface ThemeService {
 
+  /**
+   * Returns the active {@link Theme}.
+   *
+   * @return the active {@link Theme}.
+   */
   Theme getTheme();
 
+  /**
+   * Registers a {@link ThemedObject} for theme change updates.
+   *
+   * @param themedObject the {@link ThemedObject} to register for theme change updates
+   */
   void addThemedObject(ThemedObject themedObject);
 }
