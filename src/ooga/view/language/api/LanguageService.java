f017a3d1237d8f116692706226d4016ef8c067f4
David Coffman <david.coffman@duke.edu>
2021-04-27 16:36:02 -0400

    Add docs for view.language

--- src/ooga/view/language/api/LanguageService.java
+++ src/ooga/view/language/api/LanguageService.java
@@ -2,7 +2,31 @@ package ooga.view.language.api;
 
 import javafx.beans.property.ReadOnlyStringProperty;
 
+/**
+ * View-facing interface for a localization support class. Uses {@link ReadOnlyStringProperty}
+ * object to store its translations; for example, a {@link javafx.scene.control.Label} could set
+ * its label according to a certain localized String with resource bundle key "TEST", instead of
+ * accessing a resource bundle directly, it would do the following:
+ *
+ * <code>
+ *   LanguageService ls = ...;
+ *   Label myTestLabel = new Label();
+ *   myTestLabel.textProperty().bind(ls.getLocalizedString("test"));
+ * </code>
+ *
+ * The {@link javafx.scene.control.Label}'s text will then change automatically when (if) the view
+ * language changes.
+ */
 public interface LanguageService {
 
+  /**
+   * Returns a {@link ReadOnlyStringProperty} whose value is bound to the current language's
+   * translation for the key parameter. The {@link ReadOnlyStringProperty} is updated whenever
+   * the application language changes.s
+   *
+   * @param s the key to search for
+   * @return a {@link ReadOnlyStringProperty} whose value is bound to the current language's
+   * translation for the key parameter, or a {@link ReadOnlyStringProperty} if no such key exists.
+   */
   ReadOnlyStringProperty getLocalizedString(String s);
 }
--- src/ooga/view/language/bundled/BundledLanguageService.java
+++ src/ooga/view/language/bundled/BundledLanguageService.java
@@ -15,6 +15,14 @@ import ooga.view.exceptions.UIServicedException;
 import ooga.view.language.api.LanguageSelectionService;
 import ooga.view.language.api.LanguageService;
 
+/**
+ * Implementation of {@link LanguageService} and {@link LanguageSelectionService}. Uses resource
+ * bundles referenced by the application language manifest and language files from the
+ * application language resources folder. See {@link LanguageService} and {@link LanguageService}
+ * for usage details.
+ *
+ * @authod David Coffman
+ */
 public class BundledLanguageService implements LanguageService, LanguageSelectionService {
 
   private static final String DEFAULT_LANGUAGE_ROOT = "resources.languages/";
@@ -24,6 +32,11 @@ public class BundledLanguageService implements LanguageService, LanguageSelectio
   private final HashMap<String, StringProperty> strings;
   private String languageName;
 
+  /**
+   * Sole constructor for {@link BundledLanguageService}.
+   *
+   * @param exceptionService the {@link ExceptionService} to which errors should be directed
+   */
   public BundledLanguageService(ExceptionService exceptionService) {
     this.strings = new HashMap<>();
     this.availableLanguages = new TreeMap<>();
@@ -31,17 +44,30 @@ public class BundledLanguageService implements LanguageService, LanguageSelectio
     this.exceptionService = exceptionService;
   }
 
+  /**
+   * Sets a new language.
+   *
+   * @param languageName the language <em>bundle</em> name, stored as a key in the return from
+   * {@link LanguageSelectionService#getAvailableLanguages()}
+   */
   @Override
   public void setLanguage(String languageName) {
     updatePropertyValues(languageName, false);
   }
 
+  /**
+   * Returns a map of available languages in the form {BUNDLE_IDENTIFIER: LOCALIZED_NAME} -- i.e.
+   * {"english" -> "English", "spanish" -> "Español"}.
+   *
+   * @return a map of available language bundle names to their localized names
+   */
   @Override
   public Map<String, String> getAvailableLanguages() {
     updateAvailableLanguages();
     return Collections.unmodifiableMap(availableLanguages);
   }
 
+  // Refreshes the list of available languages by searching the language manifest.
   private void updateAvailableLanguages() {
     availableLanguages.clear();
 
@@ -58,10 +84,12 @@ public class BundledLanguageService implements LanguageService, LanguageSelectio
     }
   }
 
+  // Loads the default language.
   private void loadDefaultLanguage() {
     updatePropertyValues(DEFAULT_LANGUAGE, true);
   }
 
+  // Updates each StringProperty when the language is updated.
   private void updatePropertyValues(String languageName, boolean initial) {
     ResourceBundle newLang;
 
@@ -90,6 +118,15 @@ public class BundledLanguageService implements LanguageService, LanguageSelectio
     this.languageName = languageName;
   }
 
+  /**
+   * Returns a {@link ReadOnlyStringProperty} whose value is bound to the current language's
+   * translation for the key parameter. The {@link ReadOnlyStringProperty} is updated whenever
+   * the application language changes.s
+   *
+   * @param s the key to search for
+   * @return a {@link ReadOnlyStringProperty} whose value is bound to the current language's
+   * translation for the key parameter, or a {@link ReadOnlyStringProperty} if no such key exists.
+   */
   @Override
   public ReadOnlyStringProperty getLocalizedString(String s) {
     if (!strings.containsKey(s)) {
