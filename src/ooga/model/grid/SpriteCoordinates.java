f1a64d4b51fdad394469752476bc0a6c92141e32
George Hong <grh17@duke.edu>
2021-04-26 23:10:30 -0600

    Add JavaDoc to classes in the grid package

--- src/ooga/model/grid/SpriteCoordinates.java
+++ src/ooga/model/grid/SpriteCoordinates.java
@@ -6,26 +6,49 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import ooga.util.Vec2;
 
-/** @author George Hong */
+/**
+ * SpriteCoordinates encode the position of Sprites for animation.
+ *
+ * @author George Hong
+ */
 public class SpriteCoordinates {
 
   private final Vec2 position;
 
+  /**
+   * Constructs an instance of this class from a provided vector
+   *
+   * @param position vector encoding the position of a Sprite
+   */
   @JsonCreator
   public SpriteCoordinates(@JsonProperty("position") Vec2 position) {
     this.position = position;
   }
 
+  /**
+   * Default constructor, assuming a position of the origin (0, 0)
+   */
   @JsonCreator
   public SpriteCoordinates() {
     this.position = Vec2.ZERO;
   }
 
+  /**
+   * Returns the position of this Sprite
+   *
+   * @return vector encoding the position of this Sprite.
+   */
   @JsonGetter
   public Vec2 getPosition() {
     return position;
   }
 
+  /**
+   * Returns the tile coordinates of this Sprite.  TileCoordinates are strictly integer, so the
+   * Sprite position is truncated.
+   *
+   * @return TileCoordinates representing the tile occupied by the SpriteCoordinates
+   */
   @JsonIgnore
   public TileCoordinates getTileCoordinates() {
     double x = position.getX();
@@ -45,6 +68,11 @@ public class SpriteCoordinates {
     return new Vec2(tileCoordinates.getX() + 0.5, tileCoordinates.getY() + 0.5);
   }
 
+  /**
+   * Converts the coordiantes into a easily printable form
+   *
+   * @return position represented as a String
+   */
   @Override
   public String toString() {
     return position.toString();
--- src/ooga/model/grid/TileCoordinates.java
+++ src/ooga/model/grid/TileCoordinates.java
@@ -18,34 +18,70 @@ public class TileCoordinates {
   private final int x;
   private final int y;
 
+  /**
+   * Constructs an instance of this class
+   *
+   * @param x x-coordinate representing the position of the Tile
+   * @param y y-coordinate representing the position of the Tile
+   */
   public TileCoordinates(@JsonProperty("x") int x, @JsonProperty("y") int y) {
     this.x = x;
     this.y = y;
   }
 
+  /**
+   * Constructs an instance of this class from a vector, truncating extra precision, if necessary
+   *
+   * @param coordinates vector representing coordinates to construct a TileCoordinate object from
+   */
   public TileCoordinates(Vec2 coordinates) {
     this((int) coordinates.getX(), (int) coordinates.getY());
   }
 
+  /**
+   * Default constructor for TileCoordinates.  Assumes Coordinates at the top left corner,
+   * corresponding to (0, 0).
+   */
   public TileCoordinates() {
     this.x = 0;
     this.y = 0;
   }
 
+  /**
+   * Converts TileCoordinates to a vector of the position
+   *
+   * @return Vector corresponding to the position of the tile coordinates
+   */
   public Vec2 toVec2() {
     return new Vec2(x, y);
   }
 
+  /**
+   * Returns the x-coordinate of this TileCoordinate object
+   *
+   * @return x-coordinate
+   */
   @JsonGetter
   public int getX() {
     return x;
   }
 
+  /**
+   * Returns the y-coordinate of this TileCoordinate object
+   *
+   * @return x-coordinate
+   */
   @JsonGetter
   public int getY() {
     return y;
   }
 
+  /**
+   * Checks to see whether two TileCoordinate objects refer to the same position on the grid
+   *
+   * @param o other TileCoordinate objects.
+   * @return whether the other TileCoordinate object refers to the same position.
+   */
   @Override
   public boolean equals(Object o) {
     if (o instanceof TileCoordinates) {
@@ -55,6 +91,11 @@ public class TileCoordinates {
     return false;
   }
 
+  /**
+   * Gets the hash code for this TileCoordinate object.
+   *
+   * @return hash code for this TileCoordinate object.
+   */
   @Override
   public int hashCode() {
     return this.x + this.y;
