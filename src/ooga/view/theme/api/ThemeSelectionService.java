be332379b52f41bbd3e0d590c17959f9c61ddf29
David Coffman <david.coffman@duke.edu>
2021-04-27 10:39:45 -0400

    Add ThemeSelectionService docs

--- src/ooga/view/theme/api/ThemeSelectionService.java
+++ src/ooga/view/theme/api/ThemeSelectionService.java
@@ -2,9 +2,25 @@ package ooga.view.theme.api;
 
 import java.util.Set;
 
+/**
+ * Configurator-facing interface on a theme management class. Allows a referencing object to set
+ * the current theme, as well as view available themes.
+ *
+ * @author David Coffman
+ */
 public interface ThemeSelectionService {
 
+  /**
+   * Sets a new theme.
+   *
+   * @param name the name of the {@link Theme} to enable
+   */
   void setTheme(String name);
 
+  /**
+   * Returns a {@link Set<String>} containing the names of all available {@link Theme}s.
+   *
+   * @return a {@link Set<String>} containing the names of all available {@link Theme}s.
+   */
   Set<String> getAvailableThemes();
 }
