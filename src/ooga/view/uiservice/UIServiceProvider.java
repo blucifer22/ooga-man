16df4751941549c0c1c22784f6c446a99bc0601d
David Coffman <david.coffman@duke.edu>
2021-04-27 09:55:34 -0400

    Add UIServiceProvider docs

--- src/ooga/view/uiservice/UIServiceProvider.java
+++ src/ooga/view/uiservice/UIServiceProvider.java
@@ -6,15 +6,47 @@ import ooga.view.internal_api.ViewStackService;
 import ooga.view.language.api.LanguageService;
 import ooga.view.theme.api.ThemeService;
 
+/**
+ * A standardized container for UI services. Allows passing one consistent set of UI services
+ * (the overwhelmingly most common use case) instead of five separate servicing instances (one
+ * for each service interface) to dependent views.
+ *
+ * @author David Coffman
+ */
 public interface UIServiceProvider {
 
+  /**
+   * Provides the referencing view with an {@link AudioService}.
+   *
+   * @return the {@link UIServiceProvider}'s {@link AudioService}.
+   */
   AudioService audioService();
 
+  /**
+   * Provides the referencing vieew with an {@link ExceptionService}.
+   *
+   * @return the {@link UIServiceProvider}'s {@link ExceptionService}.
+   */
   ExceptionService exceptionService();
 
+  /**
+   * Provides the referencing vieew with an {@link ThemeService}.
+   *
+   * @return the {@link UIServiceProvider}'s {@link ThemeService}.
+   */
   ThemeService themeService();
 
+  /**
+   * Provides the referencing vieew with an {@link LanguageService}.
+   *
+   * @return the {@link UIServiceProvider}'s {@link LanguageService}.
+   */
   LanguageService languageService();
 
+  /**
+   * Provides the referencing vieew with an {@link ViewStackService}.
+   *
+   * @return the {@link UIServiceProvider}'s {@link ViewStackService}.
+   */
   ViewStackService viewStackManager();
 }
