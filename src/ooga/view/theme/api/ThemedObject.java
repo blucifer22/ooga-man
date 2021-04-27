0f52e8f8751edb4e8e68fdad2a9b4f0bfe277dc8
David Coffman <david.coffman@duke.edu>
2021-04-27 10:31:10 -0400

    Add ThemedObject docs

--- src/ooga/view/theme/api/ThemedObject.java
+++ src/ooga/view/theme/api/ThemedObject.java
@@ -1,6 +1,16 @@
 package ooga.view.theme.api;
 
+/**
+ * An observer interface responsible for notifying themed views when the theme changes, in order
+ * to allow them to change their appearance.
+ *
+ * @author David Coffman
+ */
 public interface ThemedObject {
 
+  /**
+   * Observer callback. Called when the theme changes. Implementing classes should re-query the
+   * {@link ThemeService} for a new {@link Theme} when this method is called.
+   */
   void onThemeChange();
 }
