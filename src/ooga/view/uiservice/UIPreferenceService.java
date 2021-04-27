e8670cd0da93cfa39df67ad91e8231ceaa4bde73
David Coffman <david.coffman@duke.edu>
2021-04-27 10:08:06 -0400

    Add view.uiservice docs

--- src/ooga/view/uiservice/UIPreferenceService.java
+++ src/ooga/view/uiservice/UIPreferenceService.java
@@ -7,6 +7,8 @@ import ooga.view.theme.api.ThemeSelectionService;
  * A standardized container for UI preference selection services. Allows passing one consistent
  * set of UI preference selection services (the overwhelmingly most common use case) instead of
  * two separate servicing instances (one for each service interface) to dependent views.
+ *
+ * @author David Coffman
  */
 public interface UIPreferenceService {
 
