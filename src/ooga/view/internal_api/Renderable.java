c19eae51b29400f3ded7edfa0f313cc4299fcfb7
David Coffman <david.coffman@duke.edu>
2021-04-27 16:46:55 -0400

    Add view.internal_api docs

--- src/ooga/view/internal_api/Renderable.java
+++ src/ooga/view/internal_api/Renderable.java
@@ -2,7 +2,20 @@ package ooga.view.internal_api;
 
 import javafx.scene.Node;
 
+/**
+ * Standardized interface for rendering views in the frontend. {@link Renderable}-implementing
+ * objects declare that they are renderable as scene components (i.e. they manage a renderable
+ * {@link javafx.scene.Node}). Used primarily in cases where view components have no "is-a"
+ * relationship with an existing JFX component (such classes are declared as extensions of JFX )
+ *
+ * @author David Coffman
+ */
 public interface Renderable {
 
+  /**
+   * Returns the {@link Renderable}'s managed {@link Node}.
+   *
+   * @return the {@link Renderable}'s managed {@link Node}.
+   */
   Node getRenderingNode();
 }
