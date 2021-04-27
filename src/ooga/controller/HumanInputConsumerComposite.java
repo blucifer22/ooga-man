2754d77d1d381da6b61a6fb117d112b2225ee67e
David Coffman <david.coffman@duke.edu>
2021-04-27 09:15:08 -0400

    HumanInputConsumerComposite docs

--- src/ooga/controller/HumanInputConsumerComposite.java
+++ src/ooga/controller/HumanInputConsumerComposite.java
@@ -5,18 +5,43 @@ import java.util.HashSet;
 import javafx.scene.input.KeyCode;
 import ooga.view.io.HumanInputConsumer;
 
+/**
+ * A composite pattern implementation for handling multiple {@link HumanInputConsumer}s. Used for
+ * game modes where multiple human input consumers need to be supplied to the
+ * {@link ooga.view.UIController}.
+ *
+ * @author David Coffman
+ */
 public class HumanInputConsumerComposite implements HumanInputConsumer {
 
   private final HashSet<HumanInputConsumer> components = new HashSet<>();
 
+  /**
+   * Default varargs constructor. Supplied with an initial varargs list of
+   * {@link HumanInputConsumer}s, which become the composite's initial consumers.
+   *
+   * @param consumers the composite's initial consumers
+   */
   public HumanInputConsumerComposite(HumanInputConsumer... consumers) {
     components.addAll(Arrays.asList(consumers));
   }
 
+  /**
+   * Adds one or more single {@link HumanInputConsumer}s to the composite.
+   *
+   * @param consumers the consumers to add to the composite
+   */
   public void addConsumers(HumanInputConsumer... consumers) {
     components.addAll(Arrays.asList(consumers));
   }
 
+  /**
+   * Removes one or more single {@link HumanInputConsumer}s to the composite. Removes only the
+   * {@link HumanInputConsumer}s actually managed by this object. Does not fail if supplied with
+   * consumers that are not managed by this object, however.
+   *
+   * @param consumers the consumers to remove from the composite
+   */
   public void removeConsumers(HumanInputConsumer... consumers) {
     Arrays.asList(consumers).forEach(components::remove);
   }
