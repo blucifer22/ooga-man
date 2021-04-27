e120508d3459a5f0d5ecfc59804e5763018a66f3
Franklin Wei <franklin@rockbox.org>
2021-04-27 18:12:28 -0400

    Javadocs for model.powerups.

--- src/ooga/model/powerups/PowerUpFactory.java
+++ src/ooga/model/powerups/PowerUpFactory.java
@@ -3,6 +3,9 @@ package ooga.model.powerups;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
+/**
+ * Factory which creates power-ups.
+ */
 public class PowerUpFactory {
 
   private static final List<PowerUp> powerUpList =
@@ -13,6 +16,10 @@ public class PowerUpFactory {
           //      new GhostSlowdownPowerUp()
           );
 
+  /**
+   * Retrieve a random power-up.
+   * @return Random power-up.
+   */
   public PowerUp getRandomPowerUp() {
     int dex = ThreadLocalRandom.current().nextInt(powerUpList.size());
     return powerUpList.get(dex);
