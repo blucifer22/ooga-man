5c46578f8d388beebe687b80f783c31f2fa7448c
David Coffman <david.coffman@duke.edu>
2021-04-27 23:09:03 -0400

    Add MenuView comments

--- src/ooga/view/views/sceneroots/MenuView.java
+++ src/ooga/view/views/sceneroots/MenuView.java
@@ -10,17 +10,33 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import ooga.view.internal_api.MainMenuResponder;
+import ooga.view.internal_api.Renderable;
 import ooga.view.internal_api.View;
+import ooga.view.theme.api.Theme;
+import ooga.view.theme.api.ThemeService;
 import ooga.view.theme.api.ThemedObject;
 import ooga.view.uiservice.UIServiceProvider;
 import ooga.view.views.components.reusable.StyledButton;
 
+/**
+ * A main menu view implementation. Provides buttons to trigger the actions in
+ * {@link MainMenuResponder} -- start the game, open the level builder, open the preferences view.
+ *
+ * @author David Coffman
+ */
 public class MenuView implements View, ThemedObject {
 
   private final GridPane primaryView;
   private final MainMenuResponder menuResponder;
   private final UIServiceProvider serviceProvider;
 
+  /**
+   * Sole constructor for {@link MenuView}.
+   *
+   * @param serviceProvider the {@link ooga.view.uiservice.ServiceProvider} to provide UI
+   *                        services as desired
+   * @param menuResponder  the {@link MainMenuResponder} to handle this view's action responses
+   */
   public MenuView(UIServiceProvider serviceProvider, MainMenuResponder menuResponder) {
     this.serviceProvider = serviceProvider;
     this.menuResponder = menuResponder;
@@ -32,6 +48,7 @@ public class MenuView implements View, ThemedObject {
     buildScene();
   }
 
+  // Add the three buttons to the scene.
   private void buildScene() {
     Label title = new Label();
     title.getStyleClass().add("menu-title");
@@ -49,17 +66,28 @@ public class MenuView implements View, ThemedObject {
     this.primaryView.add(menuButtons, 0, 1);
   }
 
+  // shorthand for an infinitely-horizontal-expanding menu button
   private Button menuButton(String labelKey, EventHandler<MouseEvent> onClickHandler) {
     Button button = new StyledButton(this.serviceProvider, labelKey, onClickHandler);
     button.setMaxWidth(Double.MAX_VALUE);
     return button;
   }
 
+  /**
+   * Returns the {@link View}'s managed {@link Pane}. Stricter requirement than that of the
+   * overridden method in {@link Renderable}, which guarantees only a {@link javafx.scene.Node}.
+   *
+   * @return the {@link View}'s managed {@link Pane}.
+   */
   @Override
   public Pane getRenderingNode() {
     return this.primaryView;
   }
 
+  /**
+   * Observer callback. Called when the theme changes. Implementing classes should re-query the
+   * {@link ThemeService} for a new {@link Theme} when this method is called.
+   */
   @Override
   public void onThemeChange() {
     this.primaryView.getStylesheets().clear();
