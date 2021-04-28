48b8eda65bdf012252a82a8a1fa5f8ef32cc15a8
David Coffman <david.coffman@duke.edu>
2021-04-27 21:39:06 -0400

    Add TileView docs

--- src/ooga/view/views/components/scenecomponents/TileView.java
+++ src/ooga/view/views/components/scenecomponents/TileView.java
@@ -5,19 +5,36 @@ import javafx.scene.Node;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.StrokeType;
+import ooga.model.api.ObservableSprite;
 import ooga.model.api.ObservableTile;
 import ooga.model.api.TileEvent;
 import ooga.model.api.TileObserver;
 import ooga.view.internal_api.Renderable;
+import ooga.view.theme.api.Costume;
+import ooga.view.theme.api.Theme;
 import ooga.view.theme.api.ThemeService;
 import ooga.view.theme.api.ThemedObject;
 
+/**
+ * Handles the rendering of a single {@link TileView}, by responding (through
+ * observation) to changes in its size and type. Also responds to theme changes and updates the
+ * view's {@link Costume} accordingly.
+ *
+ * @author David Coffman
+ */
 public class TileView implements Renderable, TileObserver, ThemedObject {
 
   private final Rectangle tileRect;
   private final ObservableTile tile;
   private final ThemeService themeService;
 
+  /**
+   * Sole {@link TileView} constructor.
+   *
+   * @param tile the {@link ObservableTile} to act as this view's data source
+   * @param tileSize the bound size of this tile
+   * @param themeService the {@link ThemeService} to query for {@link Costume}s
+   */
   public TileView(ObservableTile tile, DoubleProperty tileSize, ThemeService themeService) {
     // Configure data source
     this.tile = tile;
@@ -42,21 +59,37 @@ public class TileView implements Renderable, TileObserver, ThemedObject {
     this.onTypeChange();
   }
 
+  // Re-sets this tile's fill when its type changes.
   private void onTypeChange() {
     this.tileRect.setFill(
         themeService.getTheme().getCostumeForObjectOfType(this.tile.getType()).getFill());
   }
 
+  /**
+   * Observer callback for {@link ThemedObject}s. Called when the theme changes. Re-queries the
+   * {@link ThemeService} for a new {@link Theme} when this method is called.
+   */
   @Override
   public void onThemeChange() {
     this.onTypeChange();
   }
 
+  /**
+   * Returns the {@link TileView}'s managed {@link Node}.
+   *
+   * @return the {@link TileView}'s managed {@link Node}.
+   */
   @Override
   public Node getRenderingNode() {
     return this.tileRect;
   }
 
+  /**
+   * Called when a tile event occurs. Updates the {@link TileView}'s state to match that of
+   * the observed {@link ObservableTile}.
+   *
+   * @param e Event that occurred.
+   */
   @Override
   public void onTileEvent(TileEvent e) {
     this.onTypeChange();
