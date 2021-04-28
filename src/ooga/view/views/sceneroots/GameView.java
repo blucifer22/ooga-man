f15235daa2d1f910ec3ea64be1595f47aa4c1766
David Coffman <david.coffman@duke.edu>
2021-04-27 22:54:22 -0400

    Add GameView docs

--- src/ooga/view/views/sceneroots/GameView.java
+++ src/ooga/view/views/sceneroots/GameView.java
@@ -16,7 +16,10 @@ import ooga.model.api.GameStateObservationComposite;
 import ooga.model.api.GameStateObserver;
 import ooga.model.api.GridRebuildObserver;
 import ooga.model.api.SpriteExistenceObserver;
+import ooga.view.internal_api.Renderable;
 import ooga.view.internal_api.View;
+import ooga.view.theme.api.Theme;
+import ooga.view.theme.api.ThemeService;
 import ooga.view.theme.api.ThemedObject;
 import ooga.view.uiservice.UIServiceProvider;
 import ooga.view.views.components.audioplayer.ViewBoundAudioPlayer;
@@ -35,6 +38,11 @@ public class GameView implements View, ThemedObject, GameStateObservationComposi
   private final ViewBoundAudioPlayer audioPlayer;
   private final UIServiceProvider serviceProvider;
 
+  /**
+   * Sole constructor for {@link GameView}.
+   *
+   * @param serviceProvider the {@link UIServiceProvider} to provide UI services as desired
+   */
   public GameView(UIServiceProvider serviceProvider) {
     this.primaryView = new GridPane();
     this.serviceProvider = serviceProvider;
@@ -46,6 +54,7 @@ public class GameView implements View, ThemedObject, GameStateObservationComposi
     addGridElements();
   }
 
+  // Configures the GameView's grid constraints
   private void configureGridConstraints() {
     ColumnConstraints cc = new ColumnConstraints();
     cc.setPercentWidth(70);
@@ -64,6 +73,7 @@ public class GameView implements View, ThemedObject, GameStateObservationComposi
     this.primaryView.getStyleClass().add("view");
   }
 
+  // Initial configuration of grid elements.
   private void addGridElements() {
     this.primaryView.add(this.scoreboardCard.getRenderingNode(), 0, 0);
     this.primaryView.add(this.gridView.getRenderingNode(), 1, 0, 1, 3);
@@ -79,31 +89,61 @@ public class GameView implements View, ThemedObject, GameStateObservationComposi
     this.primaryView.add(buttonBox, 0, 1);
   }
 
+  /**
+   * Returns this composite's {@link SpriteExistenceObserver}.
+   *
+   * @return this composite's {@link SpriteExistenceObserver}.
+   */
   @Override
   public SpriteExistenceObserver spriteExistenceObserver() {
     return this.gridView;
   }
 
+  /**
+   * Returns this composite's {@link GridRebuildObserver}.
+   *
+   * @return this composite's {@link GridRebuildObserver}.
+   */
   @Override
   public GridRebuildObserver gridRebuildObserver() {
     return this.gridView;
   }
 
+  /**
+   * Returns this composite's {@link AudioObserver}.
+   *
+   * @return this composite's {@link AudioObserver}.
+   */
   @Override
   public AudioObserver audioObserver() {
     return this.audioPlayer;
   }
 
+  /**
+   * Returns this composite's {@link GameStateObserver}.
+   *
+   * @return this composite's {@link GameStateObserver}.
+   */
   @Override
   public GameStateObserver gameStateObserver() {
     return this.scoreboardCard;
   }
 
+  /**
+   * Returns the {@link GameView}'s managed {@link Pane}. Stricter requirement than that of the
+   * overridden method in {@link Renderable}, which guarantees only a {@link javafx.scene.Node}.
+   *
+   * @return the {@link GameView}'s managed {@link Pane}.
+   */
   @Override
   public Pane getRenderingNode() {
     return this.primaryView;
   }
 
+  /**
+   * Observer callback. Called when the theme changes. Implementing classes should re-query the
+   * {@link ThemeService} for a new {@link Theme} when this method is called.
+   */
   @Override
   public void onThemeChange() {
     this.primaryView.getStylesheets().clear();
