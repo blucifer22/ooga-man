1df130f716c929f370747a44c399bc0678d5aa7f
David Coffman <david.coffman@duke.edu>
2021-04-27 21:31:00 -0400

    Add SpriteView docs

--- src/ooga/view/views/components/scenecomponents/SpriteView.java
+++ src/ooga/view/views/components/scenecomponents/SpriteView.java
@@ -11,11 +11,16 @@ import ooga.model.grid.SpriteCoordinates;
 import ooga.util.Vec2;
 import ooga.view.internal_api.Renderable;
 import ooga.view.theme.api.Costume;
+import ooga.view.theme.api.Theme;
 import ooga.view.theme.api.ThemeService;
 import ooga.view.theme.api.ThemedObject;
 
 /**
- * SpriteView handles the rendering of a single Sprite (SpriteObservable, technically).
+ * Handles the rendering of a single {@link ObservableSprite}, by responding (through
+ * observation) to changes in its rotation, visibility, and type. Also responds to theme changes
+ * and updates the view's {@link Costume} accordingly.
+ *
+ * @author David Coffman
  */
 public class SpriteView implements SpriteObserver, ThemedObject, Renderable {
 
@@ -25,6 +30,13 @@ public class SpriteView implements SpriteObserver, ThemedObject, Renderable {
   private final ThemeService themeService;
   private Costume costume;
 
+  /**
+   * Sole {@link SpriteView} constructor.
+   *
+   * @param so the {@link ObservableSprite} to act as this view's data source
+   * @param themeService the {@link ThemeService} to query for a {@link Costume}
+   * @param size the bound size of this view
+   */
   public SpriteView(ObservableSprite so, ThemeService themeService, DoubleProperty size) {
     // configure data sourcing
     so.addObserver(this);
@@ -48,6 +60,12 @@ public class SpriteView implements SpriteObserver, ThemedObject, Renderable {
     updateVisibility();
   }
 
+  /**
+   * Called when a sprite event occurs. Updates the {@link SpriteView}'s state to match that of
+   * the observed {@link ObservableSprite}.
+   *
+   * @param e Event that occurred.
+   */
   @Override
   public void onSpriteUpdate(SpriteEvent e) {
     switch (e.getEventType()) {
@@ -58,11 +76,16 @@ public class SpriteView implements SpriteObserver, ThemedObject, Renderable {
     }
   }
 
+  /**
+   * Observer callback for {@link ThemedObject}s. Called when the theme changes. Re-queries the
+   * {@link ThemeService} for a new {@link Theme} when this method is called.
+   */
   @Override
   public void onThemeChange() {
     updateType();
   }
 
+  // Changes the SpriteView's costume in response to a type change event.
   private void updateType() {
     this.costume = themeService.getTheme().getCostumeForObjectOfType(dataSource.getCostume());
 
@@ -74,6 +97,7 @@ public class SpriteView implements SpriteObserver, ThemedObject, Renderable {
     this.updatePosition();
   }
 
+  // Updates the SpriteView's position in response to a position change event.
   private void updatePosition() {
     SpriteCoordinates coordinates = dataSource.getCoordinates();
     this.viewGraphic.translateXProperty()
@@ -82,6 +106,7 @@ public class SpriteView implements SpriteObserver, ThemedObject, Renderable {
         .bind(size.multiply(coordinates.getPosition().getY() - 0.5 * this.costume.getScale()));
   }
 
+  // Updates the SpriteView's orientation in response to a rotation event.
   private void updateOrientation() {
     double rotation = 0;
     double scaleX = 1;
@@ -99,10 +124,16 @@ public class SpriteView implements SpriteObserver, ThemedObject, Renderable {
     this.viewGraphic.setRotate(rotation);
   }
 
+  // Updates the SpriteView's visibility in response to a visibility change event.
   private void updateVisibility() {
     viewGraphic.setVisible(dataSource.isVisible());
   }
 
+  /**
+   * Returns the {@link SpriteView}'s managed {@link Node}.
+   *
+   * @return the {@link SpriteView}'s managed {@link Node}.
+   */
   @Override
   public Node getRenderingNode() {
     return this.viewGraphic;
